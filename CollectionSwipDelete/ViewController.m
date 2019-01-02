c4c22a0ed240ce91bce56adb6a410c5258e3c6cb
Brian.Chu <chuchengpeng0119@163.com>
2019-01-02 10:54:32 +0800

    解决删除数组越界崩溃问题
    解决删除数组越界崩溃问题

--- CollectionSwipDelete/ViewController.m
+++ CollectionSwipDelete/ViewController.m
@@ -12,7 +12,7 @@
 
 @interface ViewController ()<UICollectionViewDataSource, UICollectionViewDelegateFlowLayout>
 @property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
-@property (nonatomic, copy) NSMutableArray *datas;
+@property (nonatomic, copy) NSMutableArray<NSMutableArray *> *datas;
 
 @end
 
@@ -21,17 +21,22 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self.collectionView registerNib:[UINib nibWithNibName:@"SwipDeleteCell" bundle:nil] forCellWithReuseIdentifier:@"SwipDeleteCell"];
-    _datas = [NSMutableArray array];
-    for (int i = 0; i < 5; i ++) {
-        [_datas addObject:[NSString stringWithFormat:@"SwipDeleteCell__%d",i]];
-    }
+    [self testDatas];
     [self.collectionView enableEdit];
     [self del];
 }
 
+- (void)testDatas {
+    NSMutableArray *marr = [NSMutableArray array];
+    for (int i = 0; i < 5; i ++) {
+        [marr addObject:[NSString stringWithFormat:@"SwipDeleteCell__%d",i]];
+    }
+    _datas = @[marr.mutableCopy, marr.mutableCopy].mutableCopy;
+}
+
 - (void)del {
     self.collectionView.delCallback = ^(NSIndexPath * _Nonnull indexPath, UICollectionView * _Nonnull collectionView) {
-        [self.datas removeObjectAtIndex:indexPath.row];
+        [self.datas[indexPath.section] removeObjectAtIndex:indexPath.item];
         [self.collectionView deleteItemsAtIndexPaths:@[indexPath]];
     };
 }
@@ -41,16 +46,16 @@
 }
 
 - (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
-    return 2;
+    return _datas.count;
 }
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
-    return _datas.count;
+    return _datas[section].count;
 }
 
 - (__kindof UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
     SwipDeleteCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"SwipDeleteCell" forIndexPath:indexPath];
-    cell.name = _datas[indexPath.row];
+    cell.name = _datas[indexPath.section][indexPath.item];
     NSLog(@"cell name: %@", cell.name);
     return cell;
 }
